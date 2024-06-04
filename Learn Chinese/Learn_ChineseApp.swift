987720672752c1afe856c3ce26838ca99c78035d
Steve Matushchak <genesiskmd@gmail.com>
2024-06-03 22:40:12 -0600

    Initial Commit

---
+++ Learn Chinese/Learn_ChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_ChineseApp.swift
+//  Learn Chinese
+//
+//  Created by Steve Howard on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct Learn_ChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
